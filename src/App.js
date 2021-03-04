5d656c6a5074c4bb13b55cee457fe79432af0a66
JJ <jj.vega86@gmail.com>
2021-03-04 08:39:50 -0500

    Refactored Accordian JSX to be a React.Fragment

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import React from "react";
 import Accordion from "./components/Accordion/Accordion";
 
 const App = (props) => {
-
-    const items = [
-        {
-            title: 'What is React?',
-            content: 'React is a front-end JavaScript framework'
-        },
-        {
-            title: 'Why use React?',
-            content: 'React is the preferred way to create SPAs right now'
-        },
-        {
-            title: 'How do you use React?',
-            content: 'Create and orchestrate components!'
-        }
-    ]
+  const items = [
+    {
+      title: "What is React?",
+      content: "React is a front-end JavaScript framework",
+    },
+    {
+      title: "Why use React?",
+      content: "React is the preferred way to create SPAs right now",
+    },
+    {
+      title: "How do you use React?",
+      content: "Create and orchestrate components!",
+    },
+  ];
   return (
     <div>
-      <Accordion items={items}/>
+      <Accordion items={items} />
     </div>
   );
 };
--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -3,7 +3,7 @@ import React from "react";
 const Accordian = ({ items }) => {
   const renderedItems = items.map((item) => {
     return (
-      <div key={item.title}>
+      <React.Fragment key={item.title}>
         <div className="title active">
           <i className="dropdown icon"></i>
           {item.title}
@@ -11,7 +11,7 @@ const Accordian = ({ items }) => {
         <div className="content active">
           <p>{item.content}</p>
         </div>
-      </div>
+      </React.Fragment>
     );
   });
 
