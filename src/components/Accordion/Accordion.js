ed028b148a463682a0b0806a35e461fff40ed447
JJ <jj.vega86@gmail.com>
2021-03-07 15:29:43 -0500

    Added setState Hook to Accordian functional component, along with conditional rendering for expanding accordian items

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,32 +1,28 @@
 import React, { useState } from "react";
 
 const Accordian = ({ items }) => {
-  const [activeIndex, setActiveIndex] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(null); // this is array destructuring
 
   const onTitleClick = (index) => {
     setActiveIndex(index);
-    console.log("Title clicked", index);
   };
+
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClick(index)}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
 
-  return (
-    <div className="ui styled accordion">
-      {renderedItems}
-      <h1>{activeIndex}</h1>
-    </div>
-  );
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordian;
