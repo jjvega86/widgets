953076ffd72f54e086471d2cb6670d72ea730be1
JJ <jj.vega86@gmail.com>
2021-03-07 16:38:48 -0500

    Added Wikipedia API call to useEffect Hook that runs after initial render and after every change to state 'term'

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
-  console.log('I RUN WITH EVERY RENDER');
 
   useEffect(() => {
-    console.log("I RUN AFTER EVERY RENDER AND AT INITIAL RENDER");
+    // when component is first rendered, run. Also run after every time term changes
+    const search = async () => {
+      await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+    };
+    search();
   }, [term]);
 
   return (
