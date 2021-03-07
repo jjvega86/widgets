c69b7be152a30c42ae32de8cd4b2247d7279435e
JJ <jj.vega86@gmail.com>
2021-03-07 15:56:15 -0500

    Added Search component with useState hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from "react";
-import Accordion from "./components/Accordion/Accordion";
+// import Accordion from "./components/Accordion/Accordion";
+// remember that Accordian is expecting an items array of objects as props
+import Search from "./components/Search/Search";
 
-const App = (props) => {
-  const items = [
-    {
-      title: "What is React?",
-      content: "React is a front-end JavaScript framework",
-    },
-    {
-      title: "Why use React?",
-      content: "React is the preferred way to create SPAs right now",
-    },
-    {
-      title: "How do you use React?",
-      content: "Create and orchestrate components!",
-    },
-  ];
+const App = () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
---
+++ src/components/Search/Search.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const Search = (props) => {
+  const [term, setTerm] = useState("");
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term:</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
