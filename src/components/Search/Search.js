5c2dad5c02313c57ffe24c44a05b751d17062fe5
JJ <jj.vega86@gmail.com>
2021-03-07 16:27:21 -0500

    Added useEffect Hook to Search.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const Search = (props) => {
+const Search = () => {
   const [term, setTerm] = useState("");
+  console.log('I RUN WITH EVERY RENDER');
+
+  useEffect(() => {
+    console.log("I RUN AFTER EVERY RENDER AND AT INITIAL RENDER");
+  }, [term]);
+
   return (
     <div>
       <div className="ui form">
