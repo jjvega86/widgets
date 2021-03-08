fb94b5963c1a884420004a30c2913753f8d65e20
JJ <jj.vega86@gmail.com>
2021-03-08 08:08:57 -0500

    Added conditional rendering to dropdown for open or closed

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+
   const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null; // null in React means don't render anything
+    }
     return (
       <div
         key={option.value}
@@ -16,11 +21,16 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select A Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>{" "}
           {/* i typically means italics in HTML but Semantic UI uses it to overlay icons */}
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
