b4385d7c959a81f6743443f9cb72082484a89639
JJ <jj.vega86@gmail.com>
2021-03-08 07:58:29 -0500

    Passed state Hook property and function to Dropdown

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 // import Accordion from "./components/Accordion/Accordion";
 // remember that Accordian is expecting an items array of objects as props
 // import Search from "./components/Search/Search";
-import Dropdown from './components/Dropdown/Dropdown';
+import Dropdown from "./components/Dropdown/Dropdown";
 
 const options = [
   {
-    label: 'The Color Red',
-    value: 'red'
+    label: "The Color Red",
+    value: "red",
   },
   {
-    label: 'The Color Green',
-    value: 'green'
+    label: "The Color Green",
+    value: "green",
   },
   {
-    label: 'A Shade of Blue',
-    value: 'blue'
-  }
-]
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Dropdown options={options} />
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 };
