0e5e43d534c91d44af4afca735f24553f9efbd4f
JJ <jj.vega86@gmail.com>
2021-03-08 07:41:35 -0500

    Added Dropdown component to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 // import Accordion from "./components/Accordion/Accordion";
 // remember that Accordian is expecting an items array of objects as props
-import Search from "./components/Search/Search";
+// import Search from "./components/Search/Search";
+import Dropdown from './components/Dropdown/Dropdown';
+
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  }
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  }
+]
 
 const App = () => {
   return (
     <div>
-      <Search />
+      <Dropdown />
     </div>
   );
 };
