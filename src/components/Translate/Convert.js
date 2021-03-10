a624adba12df08542094110bfb40f81c2f82ce62
JJ <jj.vega86@gmail.com>
2021-03-10 13:02:09 -0500

    Finished setting up state Hooks to display converted text from Google Translate API request

--- src/components/Translate/Convert.js
+++ src/components/Translate/Convert.js
@@ -2,22 +2,30 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Convert = ({ language, text }) => {
+  const [convertedText, setConvertedText] = useState("");
   useEffect(() => {
-    axios.post(
-      "https://translation.googleapis.com/language/translate/v2",
-      {},
-      {
-        // empty second parameter. Nothing sent in the body
-        // third parameter sends along query string parameters
-        params: {
-          q: text,
-          target: language.value,
-          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-        },
-      }
-    );
+    axios
+      .post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          // empty second parameter. Nothing sent in the body
+          // third parameter sends along query string parameters
+          params: {
+            q: text,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      )
+      .then((res) => {
+        setConvertedText(res.data.data.translations[0].translatedText);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, [language, text]);
-  return <div></div>;
+  return <h1>{convertedText}</h1>;
 };
 
 export default Convert;
